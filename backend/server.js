7fd8c8b6064a45f49ac7df1dacf73350986aaa6e
unknown <arshadking7163@gmail.com>
2026-07-05 16:24:40 +0530

    cors fix

--- backend/server.js
+++ backend/server.js
@@ -50,7 +50,7 @@ app.use(
     },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "cache-control"],
   })
 );
 
